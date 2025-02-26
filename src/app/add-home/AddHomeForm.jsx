018b915414773c25251687bbdb347ddf0507e1c6
smitbhuva15 <itssmit123@gmail.com>
2025-02-27 00:27:23 +0530

    form validation added

--- src/app/add-home/AddHomeForm.jsx
+++ src/app/add-home/AddHomeForm.jsx
@@ -1,15 +1,59 @@
+"use client"
+import { useForm } from "react-hook-form";
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import React from 'react'
+import React, { useState } from 'react'
 import { countries } from '@/config/countries'
 import { Textarea } from '@/components/ui/textarea'
 import { Button } from '@/components/ui/button'
 import { categories } from '@/config/categories'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import toast from "react-hot-toast";
 
 export default function AddHomeForm() {
+
+    const [loading, setLoading] = useState(false);
+    const schema = yup.object().shape({
+      title: yup.string().min(3, "Title must be at least 3 characters").required("Title is required"),
+      country: yup.string().required("Please select your country"),
+      state: yup.string().required("state is required"),
+      city: yup.string().required("city is required"),
+      price: yup.string().required("Price is required"),
+      image: yup.mixed().required("Image is required"),
+      description: yup.string().min(10, "Description must be at least 10 characters").required("Description is required"),
+      
+    }).required();
+    
+
+  const { register, handleSubmit, watch, formState: { errors }, reset } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const handleFormError = () => {
+    Object.values(errors).forEach((error) => {
+      toast.error(error.message);
+    });
+  };
+
+  const onSubmit = async (data) => {
+   console.log(data,"data")
+
+   if( data.categories===false|| data.categories.length===0 ){
+     toast.error("Please select at least one category")
+   }
+   
+   if(data.image.length===0){
+    toast.error("Image is required")
+
+   }
+   
+
+}
+
     
   return (
-   <form className="mb-10">
+   <form className="mb-10"  onSubmit={handleSubmit(onSubmit,handleFormError)}>
       <div className="grid grid-cols-1  lg:grid-cols-2  gap-4">
       <div className="mt-3">
           <Label htmlFor="title" >Title</Label>
@@ -17,10 +61,9 @@ export default function AddHomeForm() {
             placeholder="Enter title"
             id="title"
             className='mt-1'
+            {...register("title")}
           />
-          <span className="text-red-500 font-bold">
-            {/* {errors?.title?.message} */}
-          </span>
+          
         </div>
 
         <div className="mt-3">
@@ -29,7 +72,7 @@ export default function AddHomeForm() {
             className="outline-brand h-10 px-3 py-2 rounded-md w-full border mt-1"
             id="countries"
 
-            // {...register("country")}
+            {...register("country")}
           >
             <option value=""> -- Select Counrties --</option>
             {countries.map((item) => (
@@ -38,31 +81,25 @@ export default function AddHomeForm() {
               </option>
             ))}
           </select>
-          <span className="text-red-500 font-bold">
-            {/* {errors?.country?.message} */}
-          </span>
+          
         </div>
 
         <div className="mt-3">
           <Label htmlFor="state">State</Label>
           <Input placeholder="Enter state" id="state" 
            className='mt-1'
-        //   {...register("state")} 
+          {...register("state")} 
           />
-          <span className="text-red-500 font-bold">
-            {/* {errors?.state?.message} */}
-          </span>
+          
         </div>
 
         <div className="mt-3">
           <Label htmlFor="city">City</Label>
           <Input placeholder="Enter city" id="city" 
            className='mt-1'
-        //   {...register("city")} 
+          {...register("city")} 
           />
-          <span className="text-red-500 font-bold">
-            {/* {errors?.city?.message} */}
-          </span>
+         
         </div>
 
         <div className="mt-3">
@@ -72,11 +109,9 @@ export default function AddHomeForm() {
             type="number"
             id="price"
              className='mt-1'
-            // {...register("price")}
+            {...register("price")}
           />
-          <span className="text-red-500 font-bold">
-            {/* {errors?.price?.message} */}
-          </span>
+          
         </div>
 
         <div className="mt-3">
@@ -87,10 +122,10 @@ export default function AddHomeForm() {
             id="image"
             accept="image/*"
              className='mt-1'
+            {...register("image")}
+
           />
-          <span className="text-red-500 font-bold">
-            {/* {errors?.image?.message} */}
-          </span>
+       
         </div>
          
       
@@ -103,11 +138,9 @@ export default function AddHomeForm() {
           id="description"
           className='resize-none mt-1'
     
-        //   {...register("description")}
+          {...register("description")}
         ></Textarea>
-        <span className="text-red-500 font-bold">
-          {/* {errors?.description?.message} */}
-        </span>
+        
       </div>
 
       <div className="mt-5">
@@ -120,6 +153,7 @@ export default function AddHomeForm() {
                 id={item.name}
                 value={item.name}
                 className="bg-red-100 border-red-300 text-red-500 focus:ring-red-200"
+                {...register("categories")}
               />
               <label htmlFor={item.name} className="text-sm font-medium ">
                 {item.name}
@@ -127,6 +161,7 @@ export default function AddHomeForm() {
             </div>
           ))}
       </div>
+    
       </div>
 
       <div className="mt-5">
