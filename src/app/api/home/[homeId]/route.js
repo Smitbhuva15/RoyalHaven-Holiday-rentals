d4aaf51318f343df18b9130871ec914c00d0b857
smitbhuva15 <itssmit123@gmail.com>
2025-02-27 01:37:52 +0530

    create and delete home api done

---
+++ src/app/api/home/[homeId]/route.js
@@ -0,0 +1,21 @@
+import prisma from '@/lib/db';
+import React from 'react'
+
+export async function DELETE(req,{params}) {
+    try {
+     const homeId=params.homeId;
+
+     const deleteHome= await prisma.home.delete({
+        where:{
+            id:homeId
+        }
+     })
+
+     return NextResponse.json({ message: "Home Deleted SuccessFully!!" }, { status: 200 });
+
+
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return NextResponse.json({ message: "Internal server error" }, { status: 500 });
+    }
+}
---
+++ src/app/api/home/addhome/route.js
@@ -0,0 +1,36 @@
+import prisma from '@/lib/db';
+import { NextResponse } from 'next/server';
+import React from 'react'
+
+export async function POST(req) {
+
+    try {
+        const { title, country, state, city, price, image_url, description, categories, userId } = await req.json();
+
+        const newhome = await prisma.home.create({
+            data: {
+                title,
+                country,
+                state,
+                city,
+                price,
+                image_url,
+                description,
+                categories,
+                userId
+            }
+        })
+
+        if(!newhome){
+        return NextResponse.json({ message: "Home is not added!!" }, { status: 400 });
+
+        }
+        return NextResponse.json({ message: "Home added successFully!!" }, { status: 200 });
+
+
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return NextResponse.json({ message: "Internal server error" }, { status: 500 });
+    }
+
+}
